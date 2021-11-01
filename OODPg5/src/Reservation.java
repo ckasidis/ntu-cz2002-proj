cfaecf5af8151b4ce06822003ea1e24c2c58d371
Kasidis Chantharojwong <77087017+ckasidis@users.noreply.github.com>
2021-11-01 18:56:53 +0800

    remove setters

--- OODPg5/src/Reservation.java
+++ OODPg5/src/Reservation.java
@@ -8,51 +8,32 @@ public class Reservation {
 	
 	//Constructors
 	public Reservation(String date, String time, int numOfPax, String name, int contactNo) {
-		this.setDate(date);
-		this.setTime(time);
-		this.setNumOfPax(numOfPax);
-		this.setName(name);
-		this.setContactNo(contactNo);
+		this.date = date;
+		this.time = time;
+		this.numOfPax = numOfPax;
+		this.name = name;
+		this.contactNo = contactNo;
 	}
 
+	//Getters and Setters
 	public String getDate() {
 		return date;
-	}
-
-	public void setDate(String date) {
-		this.date = date;
 	}
 
 	public String getTime() {
 		return time;
 	}
 
-	public void setTime(String time) {
-		this.time = time;
-	}
-
 	public int getNumOfPax() {
 		return numOfPax;
-	}
-
-	public void setNumOfPax(int numOfPax) {
-		this.numOfPax = numOfPax;
 	}
 
 	public String getName() {
 		return name;
 	}
 
-	public void setName(String name) {
-		this.name = name;
-	}
-
 	public int getContactNo() {
 		return contactNo;
 	}
 
-	public void setContactNo(int contactNo) {
-		this.contactNo = contactNo;
-	}
-	
 }
--- OODPg5/src/Staff.java
+++ OODPg5/src/Staff.java
@@ -7,10 +7,10 @@ public class Staff {
 	
 	//Constructors
 	public Staff(String name, String gender, int employeeID, String jobTitle) {
-		this.setName(name);
-		this.setGender(gender);
-		this.setEmployeeID(employeeID);
-		this.setJobTitle(jobTitle);
+		this.name  = name;
+		this.gender = gender;
+		this.employeeID = employeeID;
+		this.jobTitle = jobTitle;
 	}
 
 	//Getters and Setters
@@ -18,33 +18,17 @@ public class Staff {
 		return name;
 	}
 
-	public void setName(String name) {
-		this.name = name;
-	}
-
 	public String getGender() {
 		return gender;
-	}
-
-	public void setGender(String gender) {
-		this.gender = gender;
 	}
 
 	public int getEmployeeID() {
 		return employeeID;
 	}
 
-	public void setEmployeeID(int employeeID) {
-		this.employeeID = employeeID;
-	}
 
 	public String getJobTitle() {
 		return jobTitle;
 	}
-
-	public void setJobTitle(String jobTitle) {
-		this.jobTitle = jobTitle;
-	}
-	
 
 }
