df6abc161fd041ed9c1d7181b5605eef0d19c5d7
Kasidis Chantharojwong <77087017+ckasidis@users.noreply.github.com>
2021-11-01 17:07:37 +0800

    add getters and setters to classes

--- OODPg5/src/Reservation.java
+++ OODPg5/src/Reservation.java
@@ -8,10 +8,50 @@ public class Reservation {
 	
 	//Constructors
 	public Reservation(String date, String time, int numOfPax, String name, int contactNo) {
+		this.setDate(date);
+		this.setTime(time);
+		this.setNumOfPax(numOfPax);
+		this.setName(name);
+		this.setContactNo(contactNo);
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
 		this.date = date;
+	}
+
+	public String getTime() {
+		return time;
+	}
+
+	public void setTime(String time) {
 		this.time = time;
+	}
+
+	public int getNumOfPax() {
+		return numOfPax;
+	}
+
+	public void setNumOfPax(int numOfPax) {
 		this.numOfPax = numOfPax;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
 		this.name = name;
+	}
+
+	public int getContactNo() {
+		return contactNo;
+	}
+
+	public void setContactNo(int contactNo) {
 		this.contactNo = contactNo;
 	}
 	
