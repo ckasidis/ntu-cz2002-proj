6dee64209297a6453c74c3180d718f94e504988b
Nusra7479 <nusrath.hajara@gmail.com>
2021-11-07 17:40:33 +0800

    Included javadoc

--- OODPg5/src/OODPg5/Dessert.java
+++ OODPg5/src/OODPg5/Dessert.java
@@ -1,11 +1,21 @@
 package OODPg5;
-
+/**
+ * Represents a dessert sold in the restaurant
+ * @author Group 6
+ *
+ */
 public class Dessert implements MenuItem {
 	private TypeOfItem item;
 	private String name;
 	private double price;
 	private String description;
 	
+	/**
+	 * Creates a dessert to be sold
+	 * @param name Name of the dessert
+	 * @param price Price of the dessert
+	 * @param description Description of the dessert
+	 */
 	public Dessert(String name, double price, String description) {
 		this.name = name;
 		this.price = price;
@@ -13,31 +23,52 @@ public class Dessert implements MenuItem {
 		item = TypeOfItem.DESSERT;	
 	}
 	
+	/**
+	 * Print the description of the dessert
+	 */
 	public void printDescription() {
 		System.out.println(this.description);
 	}
 	//getters
+	/**
+	 * Get the name of the dessert
+	 */
 	public String getName() {
 		return name;
 	}
 	
+	/**
+	 * Get the price of the dessert
+	 */
 	public double getPrice() {
 		return price;
 	}
 	
+	/**
+	 * Get the type of item (Dessert)
+	 */
 	public TypeOfItem getItemType() {
 		return item;
 	}
 	
 	//setters
+	/**
+	 * Set the name of dessert
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
+	/**
+	 * Set the price of the dessert
+	 */
 
 	public void setPrice(double price) {
 		this.price = price;
 	}
 	
+	/**
+	 * Set the description for the dessert
+	 */
 	public void setDescription(String description) {
 		this.description = description;
 	}
--- OODPg5/src/OODPg5/TimeSlot.java
+++ OODPg5/src/OODPg5/TimeSlot.java
@@ -4,35 +4,79 @@ import java.time.LocalTime;
 import java.util.ArrayList;
 import java.time.LocalDate;//
 
-
+/**
+ * Represent a time slot during restaurant opening hours
+ * 
+ * @author Group 6
+ *
+ */
 public class TimeSlot {
+	/**
+	 * Starting time of time slot
+	 */
 	private LocalTime startTime;
+	
+	/**
+	 * Ending time of time slot
+	 */
 	private LocalTime endTime;
+	
+	/**
+	 * List of reservations
+	 */
 	private ArrayList<Reservation> reservationList = new ArrayList<Reservation>();
 	
 	//constructor
+	/**
+	 * Creates a time slot
+	 * 
+	 * @param startTime Starting time of the time slot
+	 * @param endTime Ending time of the time slot
+	 */
 	public TimeSlot(LocalTime startTime, LocalTime endTime) {
 		this.startTime = startTime;
 		this.endTime = endTime;
 	}
 	
 	//getters
+	/**
+	 * Get the starting time of the time slot
+	 * @return Starting time of the time slot
+	 */
 	public LocalTime getStartTime() {
 		return startTime;
 	}
+	
+	/**
+	 * Get the ending time of time slot
+	 * @return Ending time of the time slot
+	 */
 	public LocalTime getEndTime() {
 		return endTime;
 	}
+	
+	/**
+	 * Get the list of reservations
+	 * @return Array list of reservations
+	 */
 	public ArrayList<Reservation> getReservationList() {
 		return reservationList;
 	} //
 	
 	//add reservation
+	/**
+	 * Add new reservation to the list of reservations
+	 * @param reservation New reservation to be added
+	 */
 	public void addReservation(Reservation reservation) {
 		reservationList.add(reservation);
 	}
 	
 	//remove reservation
+	/**
+	 * Remove the reservation on specified date from the list of reservations
+	 * @param date Date of reservation to be removed
+	 */
 	public void removeReservation(LocalDate date) {
 		if (!reservationList.isEmpty()) {
 			for (Reservation res : reservationList) {
