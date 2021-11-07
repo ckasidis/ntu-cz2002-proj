d454829e72a1d5f9588e61442c7ef48fac8a8351
JingYen <79895010+wJingYen@users.noreply.github.com>
2021-11-07 19:14:38 +0800

    Update Order.java

--- OODPg5/src/OODPg5/Order.java
+++ OODPg5/src/OODPg5/Order.java
@@ -1,105 +1,94 @@
 package OODPg5;
 
 import java.util.ArrayList;
-import java.util.Scanner;
 import java.util.Collections;
-
-import java.util.Calendar;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Order {
+	static int checkNo =0;
+	private ArrayList<MenuItem> orderItem;
 
-	ArrayList<MenuItem> menuItem;
-
-	Calendar c = Calendar.getInstance();
-
-	private String customerName;
+	private Customer customer;
 	private Staff staff;
-	private MenuItem[] orderedItem;
-	private Table table;
-	private String strDate;
+	private int table_no;
+	private int cNumber;
+	private double finalCheck;
 
-
-	public Order(String customerName, Staff staff, MenuItem[] orderedItem, Table table, String strDate) {
-		this.customerName = customerName;
+//An order should indicate the staff who created the order.
+//Order invoice can be printed to list the order details (eg, table number, timestamp)
+//	and a complete breakdown of order items details with taxes details.
+	
+	public Order(Customer customer, Staff staff, int tableNo) {
+		this.customer = customer;
 		this.staff = staff;
-		this.orderedItem = orderedItem;
-		this.table = table;
-
-		Date date = c.getTime();
-		//DateFormat dateFormat = new SimpleDateFormat("dd MMMM yyyy"); //Eg 13 April 2015
-		//DateFormat dateFormat = new SimpleDateFormat("dd MMMM"); //Eg 13 April
-		DateFormat dateFormat = new SimpleDateFormat("dd-M-yyyy); //Eg 13-4-2015
-		this.strDate = dateFormat.format(date);
+		table_no = tableNo;
+		cNumber = checkNo++;
+		finalCheck =0;
 	}
-
-
-	public void addOrder(MenuItem) {
-		menuItem.add(MenuItem);
+	public void addOrder(MenuItem mi) {
+		orderItem.add(mi);
+		sort(orderItem);		
 	}
-
-	public void removeOrder(MenuItem) {
-		menuItem.remove(MenuItem);
+	public void removeOrder(MenuItem mi) {
+		orderItem.remove(mi);
 	}
-
 	public void viewOrder() {
-		int temp = 1;
-		for (int i=0;i<menuItem.size();i++) {
-			System.out.println("Order #"+temp+" is "+menuItem.get(i).getName);
+		int temp = 1, count=0;
+		MenuItem tm = orderItem.get(0);
+		for (int i=1;i<orderItem.size();i++){
+			if(tm != orderItem.get(i)) {
+				System.out.printf("Order #%d: %d %s \t %.2f",temp++,count,orderItem.get(i).getName(),orderItem.get(i).getPrice());
+				count =0;
+				tm = orderItem.get(i);
+			}
+			count++;
 		}
 	}
-
-	public void printOrderInvoice() {
-		int temp = 1;
-		int sum = 0;
-
-		System.out.println("Table No. "+ table);
-		System.out.println("Time: "+ c.get(Calendar.DATE)+"/"+ c.get(Calendar.MONTH)+"/"+c.get(Calendar.YEAR)+" --- "+ c.get(Calendar.HOUR)+":"+c.get(Calendar.MINUTE))
-		System.out.println("Your waiter for today is: "+ staff);
-
-		System.out.println("");
-
-		for (int i=0;i<menuItem.size();i++) {
-			sum = sum + menuItem.get(i).getPrice;
-			System.out.println("Order #" + temp + " is " + menuItem.get(i).getName() + " --- $"+ menuItem.get(i).getPrice());
-			temp++;
-		}
-
-		System.out.println("");
-
-		System.out.println("Subtotal: $"+sum+" SGD");
-		//Check for membership
-			//if/else apply discount
-		//if (customer.getDiscount())  ***Currently no "assign/unassign" membership to customer so doing it this way***
-		int discount = sum*customer.getDiscount();
-		sum = sum - discount;
-
-		//Add GST
-		sum = sum * 1.07; //7%
-
-		System.out.println("Total bill: $"+sum+" SGD");
-		System.out.println("Thank you, "+ customerName + ", for dining here.");
-
-		/** Example Invoice
-		Table no: xx
-		Time: date/month/year --- hour/minutes
-		Waiter: yyy
-
-		Order #z abc ---$xx
-		Order #z abc ---$xx
-		Order #z abc ---$xx
-
-		Subtotal: $xxx SGD
-		Total bill: $xxx SGD
-		Thank you, xyz, for dining here.
-		**/
-
-	}
-
 	public int getTableNum() {
-		return table;
+		return table_no;
+	}
+	private void sort(ArrayList<MenuItem> menuItemList) {
+		int prev;
+		if(menuItemList.size()<=1) return;
+		for (int i=0;i<orderItem.size()-1;i++) {
+			prev = i+1;
+			for (int j=i+1; j<orderItem.size();j++) {
+				if(orderItem.get(i) == orderItem.get(j)) {
+					Collections.swap(orderItem, prev, j);
+				}
+			  }
+		}
+	}
+	public void printOrderInvoice() {
+		LocalDateTime date = LocalDateTime.now();
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("E dd/MM/yyyy HH:mm");
+		double temp = 1;
+		double sum = 0;
+		System.out.println("            Restaurant RRPSS             ");
+		System.out.println("*****************************************");
+		System.out.println("check No." + cNumber);
+		System.out.println("Server: " + staff.getName() + "        Table:" + table_no);
+		System.out.println(date.format(dtf));
+		System.out.println("-----------------------------------------");
+		viewOrder();
+		System.out.println("-----------------------------------------");
+		for(MenuItem mi: orderItem) {
+			sum = sum + mi.getPrice();
+		}
+		temp = sum;
+		System.out.printf("                 Subtotal:    $%.2f SGD\n", temp = sum);
+		System.out.printf("         10% SERVICE CHRG:    $%.2f SGD\n", sum*0.1);
+		temp = temp + sum*0.1;
+		System.out.printf("         MEMBER DISCOUNTS:    $%.2f SGD\n", customer.getDiscount(sum));
+		System.out.println("-----------------------------------------");
+		finalCheck = temp-customer.getDiscount(sum);
+		System.out.printf("                    TOTAL:    $%.2f SGD\n", finalCheck);
+		System.out.println("-----------------------------------------");
+		System.out.println("*****************************************");
+		System.out.println("**    Thank you for dining with us!    **");
+		System.out.println("*****************************************");
+
 	}
 
 }
