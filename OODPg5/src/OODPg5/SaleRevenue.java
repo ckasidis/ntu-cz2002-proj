4f6319e283bdf7618a2a540ea760a99a323ea98c
JingYen <79895010+wJingYen@users.noreply.github.com>
2021-11-10 16:42:34 +0800

    Update SaleRevenue.java

--- OODPg5/src/OODPg5/SaleRevenue.java
+++ OODPg5/src/OODPg5/SaleRevenue.java
@@ -1,101 +1,81 @@
 package OODPg5;
-
 import java.util.ArrayList;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
 
-import java.util.Calendar;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-/**
- * Represents the sales revenue of the restaurant
- * @author Group 5
- *
- */
 public class SaleRevenue {
-	/**
-	 * Array list of menu items
-	 */
 
-	ArrayList<MenuItem> menuItem;
+	private ArrayList<ArrayList<Order>> year = new ArrayList<ArrayList<Order>>();
+	
 
-	Calendar c = Calendar.getInstance();
-	/**
-	 * Total revenue from the restaurant
-	 */
 	private Double totalRevenue;
-	
-	/**
-	 * List of orders placed in the restaurant
-	 */
-	private Order orders[];
-	/**
-	 * Today's date in 'dd-mm-yyyy' format
-	 */
+
 	private String checkToday;
-	/**
-	 * Today's month in 'mm-yyyy' format
-	 */
 	private String checkMonth;
-	
-	/**
-	 * Creates sales revenue of the restaurant
-	 * @param totalRevenue Total revenue from the restaurant
-	 * @param orders  List of orders placed in the restaurant
-	 */
-	public SaleRevenue(double totalRevenue, Order orders[]) {
-		this.totalRevenue = totalRevenue;
-		this.orders = orders;
-	}
-	/**
-	 * Calculate the sales revenue of the restaurant for today or this month
-	 * @param isToday TRUE if calculating today's revenue, FALSE if calculating this month's revenue
-	 * @return Sales Revenue of the restaurant for today or this month
-	 */
-	public Double calculateSaleRevenue(boolean isToday) { //int check to see if Today or Month, 1 for Today, 0 for Month
-
-		//For checking Today's sale, match all 3 (date,month,year)
-		Date dateToday = c.getTime();
-		//DateFormat dateFormat = new SimpleDateFormat("dd MMMM yyyy"); //Eg 13 April 2015
-		//DateFormat dateFormat = new SimpleDateFormat("dd MMMM"); //Eg 13 April
-		DateFormat dateFormatToday = new SimpleDateFormat("dd-M-yyyy"); //Eg 13-4-2015
-		this.checkToday = dateFormatToday.format(dateToday);
-
-		//For checking Month's sale, match all 2 (month,year)
-		Date dateMonth = c.getTime();
-		//DateFormat dateFormat = new SimpleDateFormat("dd MMMM yyyy"); //Eg 13 April 2015
-		//DateFormat dateFormat = new SimpleDateFormat("dd MMMM"); //Eg 13 April
-		DateFormat dateFormatMonth = new SimpleDateFormat("M-yyyy"); //Eg 4-2015
-		this.checkMonth = dateFormatMonth.format(dateMonth);
 
 
-		if(isToday == true){ //boolean check for today or month
-
-			//Revenue for the Today
-			double sum = 0;
-
-			for(int i=0;i<orders.length;i++) {
-				if(orders.strDate == checkToday) { // if date and month match, print and sum the orders
-					sum = sum + orders[i].orderedItem.getPrice();
-				}
-			}
-
-			return sum;
-		}else {
-
-			String formatMonth = orders.strDate.substring(3,9); //Eg Cut out "13-" from "13-4-2015"
-
-			//Revenue for the Month
-			Double sum = 0;
-
-			for(int i=0;i<orders.length;i++) {
-				if(formatMonth == checkMonth) { // if date and month match, print and sum the orders
-					sum = sum + orders[i].orderedItem.getPrice();
-				}
-			}
-
-			return sum;
+	public SaleRevenue() {
+		System.out.println("Record up to 1 year of Sales");
+		for(int i=0;i<12;i++) {
+			year.add(new ArrayList<Order>());
 		}
-
 	}
-
+	public void addOrder(Order order) {
+		int mnth = order.getDate().getMonthValue()-1;
+		year.get(mnth).add(order);
+	}
+	public void printSalesRevenueDay(LocalDate date) {
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		System.out.println("Date:" + date.format(dtf));
+		
+		int mnth = date.getMonthValue()-1;
+		double totalRevenue =0;
+		for(Order o: year.get(mnth)) {
+			if(o.getDate().compareTo(date)==0)
+				o.viewOrder();
+				totalRevenue += o.getfinalPrice();
+		}
+		System.out.println("Total Revenue for the day:"+totalRevenue);
+	}
+	public void printSalesRevenueMonth(LocalDate date) {
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("MM/yyyy");
+		DateTimeFormatter dtf1 = DateTimeFormatter.ofPattern("dd/MM");
+		System.out.println("Date:" + date.format(dtf));
+		
+		LocalDate tempDate = date.withDayOfMonth(1);
+		int mnth = date.getMonthValue()-1;
+		double totalRevenue =0;
+		for(Order o: year.get(mnth-1)) {
+			if(o.getDate().compareTo(tempDate)!=0) {
+				System.out.println("Date:" + date.format(dtf1));
+				tempDate = o.getDate();
+			}
+			o.viewOrder();
+			totalRevenue += o.getfinalPrice();
+		}
+		System.out.println("Total Revenue for the Month:"+totalRevenue);
+	}
+	public void printSalesRevenueYear() {
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		DateTimeFormatter dtf1 = DateTimeFormatter.ofPattern("MM/yyyy");
+		LocalDate tempDate = LocalDate.now();
+		 LocalDate tempDate1= tempDate.minusYears(1);
+		 double totalRevenue = 0;
+		 System.out.println("Printing Sales Record from :"+ tempDate1.format(dtf)+ "to " + tempDate.format(dtf));
+		 int i=0;
+		for(ArrayList<Order> mnth : year) {
+			if(mnth.size()!=0) {
+				System.out.println("Month: " +  mnth.get(0).getDate().format(dtf1));
+				for(Order o: mnth) {
+					tempDate = o.getDate().withDayOfMonth(1);
+					if(o.getDate().compareTo(tempDate)!=0) {
+						System.out.println("Date:" + o.getDate().format(dtf1));
+						tempDate = o.getDate();
+					}
+					o.viewOrder();
+					totalRevenue += o.getfinalPrice();
+				}
+			}
+		}
+	}
 }
