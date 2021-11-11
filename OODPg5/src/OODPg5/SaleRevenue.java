6abc31552887274e64a2686a5a894ad17047b77c
JingYen <79895010+wJingYen@users.noreply.github.com>
2021-11-11 19:52:54 +0800

    Update SaleRevenue.java

--- OODPg5/src/OODPg5/SaleRevenue.java
+++ OODPg5/src/OODPg5/SaleRevenue.java
@@ -44,11 +44,14 @@ public class SaleRevenue {
 		double totalRevenue =0;
 		for(Order o: year.get(mnth)) {
 			if(o.getDate().compareTo(date)==0)
+				System.out.println("");
 				System.out.println("Date:" + o.getDate().format(dtf));
+				
 				o.viewOrder();
 				totalRevenue += o.getfinalPrice();
 		}
-		System.out.println("Total Revenue for the day:"+totalRevenue);
+		System.out.println("");
+		System.out.printf("Total Revenue for the day: %.2f\n",totalRevenue);
 	}
 	
 	/**
@@ -62,15 +65,17 @@ public class SaleRevenue {
 		LocalDate tempDate = date.withDayOfMonth(1);
 		int mnth = date.getMonthValue()-1;
 		double totalRevenue =0;
-		for(Order o: year.get(mnth-1)) {
+		for(Order o: year.get(mnth)) {
 			if(o.getDate().compareTo(tempDate)!=0) {
+				System.out.println("");
 				System.out.println("Date:" + date.format(dtf));
 				tempDate = o.getDate();
 			}
 			o.viewOrder();
 			totalRevenue += o.getfinalPrice();
 		}
-		System.out.println("Total Revenue for the Month:"+totalRevenue);
+		System.out.println("");
+		System.out.printf("Total Revenue for the Month: %.2f\n",totalRevenue);
 	}
 	
 	/**
@@ -83,13 +88,14 @@ public class SaleRevenue {
 		 LocalDate tempDate1= tempDate.minusYears(1);
 		 double totalRevenue = 0;
 		 System.out.println("Printing Sales Record from :"+ tempDate1.format(dtf)+ "to " + tempDate.format(dtf));
-		 int i=0;
 		for(ArrayList<Order> mnth : year) {
 			if(mnth.size()!=0) {
+				System.out.println("");
 				System.out.println("Month: " +  mnth.get(0).getDate().format(dtf1));
 				for(Order o: mnth) {
 					tempDate = o.getDate().withDayOfMonth(1);
 					if(o.getDate().compareTo(tempDate)!=0) {
+						System.out.println("");
 						System.out.println("Date:" + o.getDate().format(dtf1));
 						tempDate = o.getDate();
 					}
@@ -98,6 +104,7 @@ public class SaleRevenue {
 				}
 			}
 		}
-		System.out.println("Total Revenue for the Year:"+totalRevenue);
+		System.out.println("");
+		System.out.printf("Total Revenue for the Year: %.2f\n",totalRevenue);
 	}
 }
